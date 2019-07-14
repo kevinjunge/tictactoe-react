e08c7cc2b66a4dc44c8df26e56124bf86e8738e4
kevin <k.vinjunge@gmail.com>
2019-07-13 18:00:44 -0700

    create Game class

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,4 @@
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      squares: Array(9).fill(null)
-      xIsNext: true,
-    }
-  }
-
   handleClick(i){
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]){
@@ -22,24 +14,15 @@ class Board extends React.Component {
   renderSquare(i){
     return(
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner){
-      status = 'Winner: '+ winner;
-    }else {
-      status = 'Next player: ' +(this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
